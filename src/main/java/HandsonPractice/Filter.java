e0180a5a5ad422e9949389fefe156205a6943a33
Ujjwal <Ujjwalrawat248@gmail.com>
2025-04-10 11:39:33 +0530

    [Author:Ujjwal] ADD:Filter.java

---
+++ src/main/java/HandsonPractice/Filter.java
@@ -0,0 +1,26 @@
+package HandsonPractice;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Filter {
+    public static void main(String[] args) {
+        System.out.println("Printing all reecords with age grater than 25");
+        ObjectMapper objectMapper=new ObjectMapper();
+        try {
+            JsonNode dataNode=objectMapper.readTree(new File("src/SampleData/Data.json"));
+            if(dataNode.isArray()){
+                for(JsonNode node:dataNode){
+                    if(node.has("age") && node.get("age").asInt()>25){
+                        System.out.println(node);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
